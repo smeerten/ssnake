A bug in subtract averages has been fixed Also some additional changes were made to the MQMAS fitting.

src/hypercomplex.py
@@ -105,7 +105,9 @@ class HComplexData(object):
         return self.__add__(other)
 
     def __sub__(self, other):
-        return self.__add__(-np.asarray(other))
+        if isinstance(other, list):
+            other = np.asarray(other)
+        return self.__add__(-other)
     
     def __rsub__(self, other):
         return (-self).__add__(other)

src/simFunctions.py
@@ -72,15 +72,13 @@ def voigtLine(x, pos, lor, gau, integral, Type=0):
         gauss = np.exp(-axis**2 / (2 * f**2)) / (f * np.sqrt(2 * np.pi))
         return integral * (eta * lor + (1 - eta) * gauss)
 
-
 def makeSpectrum(x, sw, v, gauss, lor, weight):
     # Takes axis, frequencies and intensities and makes a spectrum with lorentz and gaussian broadening
     length = len(x)
-    t = np.arange(length) / sw
+    t = np.abs(np.fft.fftfreq(length, sw/float(length)))
     diff = (x[1]-x[0])*0.5
     final, junk = np.histogram(v, length, range=[x[0]-diff, x[-1]+diff], weights=weight)
     apod = np.exp(-np.pi * np.abs(lor) * t -((np.pi * np.abs(gauss) * t)**2) / (4 * np.log(2)))
-    apod[-1:-int(len(apod) / 2 + 1):-1] = apod[:int(len(apod) / 2)]
     inten = np.real(np.fft.fft(np.fft.ifft(final) * apod))
     inten = inten / sw * len(inten) 
     return inten
@@ -88,17 +86,15 @@ def makeSpectrum(x, sw, v, gauss, lor, weight):
 def makeSpectrum2d(x, sw, v, gauss, lor, weight):
     # Takes axis, frequencies and intensities and makes a spectrum with lorentz and gaussian broadening in 2 dimensions
     length1 = len(x[0])
-    t1 = np.arange(length1) / sw[0]
+    t1 = np.abs(np.fft.fftfreq(length1, sw[0]/float(length1)))
     diff1 = (x[0][1]-x[0][0])*0.5
     length2 = len(x[1])
-    t2 = np.arange(length2) / sw[1]
+    t2 = np.abs(np.fft.fftfreq(length2, sw[1]/float(length2)))
     diff2 = (x[1][1]-x[1][0])*0.5
     final, junk, junk = np.histogram2d(v[0], v[1], [length1,length2], range=[[x[0][0]-diff1, x[0][-1]+diff1],[x[1][0]-diff2, x[1][-1]+diff2]], weights=weight)
     apod1 = np.exp(-np.pi * np.abs(lor[0]) * t1 -((np.pi * np.abs(gauss[0]) * t1)**2) / (4 * np.log(2)))
-    apod1[-1:-int(len(apod1) / 2 + 1):-1] = apod1[:int(len(apod1) / 2)]
     apod1 = apod1[:, np.newaxis]
     apod2 = np.exp(-np.pi * np.abs(lor[1]) * t2-((np.pi * np.abs(gauss[1]) * t2)**2) / (4 * np.log(2)))
-    apod2[-1:-int(len(apod2) / 2 + 1):-1] = apod2[:int(len(apod2) / 2)]
     inten = np.real(np.fft.fft2(np.fft.ifft2(final) * apod1 * apod2))
     inten = inten / sw[0] * inten.shape[0] / sw[1] * inten.shape[1]
     return inten
@@ -282,13 +278,13 @@ def mqmasAngleStuff(cheng):
 def mqmasFreq(p, I, vCS, Cq, eta, angleStuff, freq):
     Cp0 = p * (I*(I+1) - 3/4.0 * p**2)
     Cp4 = p * (18*I*(I+1) - 17/2.0 * p**2 - 5)
-    v0Q = - Cq**2 / (10.0 * freq) / (2*I*(I-1))**2 * (eta**2 + 3)
-    v4Q = Cq**2 / (1120.0 * freq) * 7 / (18.0 * (2*I*(I-1))**2) * (angleStuff[0] + angleStuff[1]*eta + angleStuff[2]*eta**2)
-    return vCS + Cp0*v0Q + Cp4*v4Q
+    v0Q = - Cq**2 / (10.0 * freq) / (2*I*(2*I-1))**2 * (eta**2 + 3)
+    v4Q = Cq**2 / (1120.0 * freq) * 7 / (18.0 * (2*I*(2*I-1))**2) * (angleStuff[0] + angleStuff[1]*eta + angleStuff[2]*eta**2)
+    return -p*vCS + Cp0*v0Q + Cp4*v4Q
 
 def mqmasFunc(x, I, p, pos, cq, eta, lor, gauss, angleStuff, freq, sw, weight):
     cq *= 1e6
-    v2 = mqmasFreq(1, 5/2.0, pos, cq, eta, angleStuff, freq[1])
-    v1 = mqmasFreq(p, 5/2.0, pos, cq, eta, angleStuff, freq[0])
+    v2 = mqmasFreq(1, I, pos, cq, eta, angleStuff, freq[1])
+    v1 = mqmasFreq(p, I, pos, cq, eta, angleStuff, freq[0])
     return makeSpectrum2d(x, sw, [v1, v2], gauss, lor, weight)
 

src/views.py
@@ -1501,8 +1501,7 @@ class CurrentContour(CurrentStacked):
         else:
             self.differ = np.max(np.abs(np.ravel(self.data.getHyperData)))
         self.ax.cla()
-        self.x_ax.cla()
-        self.y_ax.cla()
+        self.clearProj()
         axMult = self.getAxMult(self.spec(), self.getAxType(), self.getppm(), self.freq(), self.ref())
         axMult2 = self.getAxMult(self.spec(-2), self.getAxType(-2), self.getppm(-2), self.freq(-2), self.ref(-2))
         if self.viewSettings["diagonalBool"]:
@@ -1513,12 +1512,14 @@ class CurrentContour(CurrentStacked):
             self.line_ydata_extra.append(self.xax(-2) * axMult2)
             self.line_zdata_extra.append(tmp)
             self.plotContour(self.line_xdata_extra[-1], self.line_ydata_extra[-1], self.line_zdata_extra[-1], color=['k','k'], alpha=0.2)
+            self.showProj(self.line_xdata_extra[-1], self.line_ydata_extra[-1], self.line_zdata_extra[-1], 'k')
         if extraX is not None:
             for num in range(len(extraX)):
                 self.line_xdata_extra.append(extraX[num] * axMult)
                 self.line_ydata_extra.append(extraY[num] * axMult2)
                 self.line_zdata_extra.append(extraZ[num])
                 self.plotContour(self.line_xdata_extra[-1], self.line_ydata_extra[-1], self.line_zdata_extra[-1], color=[extraColor[num], extraColor[num]])
+                self.showProj(self.line_xdata_extra[-1], self.line_ydata_extra[-1], self.line_zdata_extra[-1], extraColor[num])
         self.line_xdata = [self.xax() * axMult]
         self.line_ydata = [self.xax(-2) * axMult2]
         self.line_zdata = [tmpdata]
@@ -1527,7 +1528,7 @@ class CurrentContour(CurrentStacked):
         else:
             self.differ = np.max(np.abs(np.ravel(self.data.getHyperData)))
         self.plotContour(self.line_xdata[-1], self.line_ydata[-1], self.line_zdata[-1])
-        self.showProj()
+        self.showProj(self.line_xdata[-1], self.line_ydata[-1], self.line_zdata[-1])
         self.ax.set_xlabel(self.getLabel(self.spec(), self.axes[-1], self.getAxType(), self.getppm()))
         self.ax.set_ylabel(self.getLabel(self.spec(-2), self.axes[-2], self.getAxType(-2), self.getppm(-2)))
         if self.spec():
@@ -1602,14 +1603,23 @@ class CurrentContour(CurrentStacked):
         if updateOnly:
             self.canvas.draw()
 
-    def showProj(self):
-        xLimOld = self.x_ax.get_xlim()
-        x = self.line_xdata[-1]  # Get plot data from plot
-        yLimOld = self.y_ax.get_ylim()
-        y = self.line_ydata[-1]  # Get plot data from plot
+    def clearProj(self):
         self.x_ax.cla()
         self.y_ax.cla()
-        tmpdata = np.real(self.getDataType(self.data1D.getHyperData(0)))
+
+    def showProj(self, line_xdata=None, line_ydata=None, line_zdata=None, color=None):
+        xLimOld = self.x_ax.get_xlim()
+        if line_xdata is None:
+            x = self.line_xdata[-1]
+            y = self.line_ydata[-1]
+            tmpdata = np.real(self.getDataType(self.data1D.getHyperData(0)))
+        else:
+            x = line_xdata
+            y = line_ydata
+            tmpdata = line_zdata
+        yLimOld = self.y_ax.get_ylim()
+        if color is None:
+            color = self.viewSettings["color"]
         Limits = self.viewSettings["projLimits"]
         topSlice = (slice(None), slice(None))
         rightSlice = (slice(None), slice(None))
@@ -1639,7 +1649,7 @@ class CurrentContour(CurrentStacked):
                 self.viewSettings["projPos"][0] = 0
             xprojdata = tmpdata[self.viewSettings["projPos"][0]]
         if self.viewSettings["projTop"] != 3:
-            self.x_ax.plot(x, xprojdata, color=self.viewSettings["color"], linewidth=self.viewSettings["linewidth"], picker=True)            
+            self.x_ax.plot(x, xprojdata, color=color, linewidth=self.viewSettings["linewidth"], picker=True)            
             xmin, xmax = np.min(xprojdata), np.max(xprojdata)
             self.x_ax.set_ylim([xmin - 0.15 * (xmax - xmin), xmax + 0.05 * (xmax - xmin)])  # Set projection limits, and force 15% whitespace below plot
             self.x_ax.set_xlim(xLimOld)
@@ -1656,7 +1666,7 @@ class CurrentContour(CurrentStacked):
                 self.viewSettings["projPos"][1] = 0
             yprojdata = tmpdata[:, self.viewSettings["projPos"][1]]
         if self.viewSettings["projRight"] != 3:
-            self.y_ax.plot(yprojdata, y, color=self.viewSettings["color"], linewidth=self.viewSettings["linewidth"], picker=True)
+            self.y_ax.plot(yprojdata, y, color=color, linewidth=self.viewSettings["linewidth"], picker=True)
             ymin, ymax = np.min(yprojdata), np.max(yprojdata)
             self.y_ax.set_xlim([ymin - 0.15 * (ymax - ymin), ymax + 0.05 * (ymax - ymin)])  # Set projection limits, and force 15% whitespace below plot
             self.y_ax.set_ylim(yLimOld)