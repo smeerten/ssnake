4c25fe63e727b7935b5c21cb7e241e4f60cb3fce
wfranssen <wmjfranssen@zoho.com>
2018-04-24 12:17:03 +0200

    Update copyright in About

--- src/ssNake.py
+++ src/ssNake.py
@@ -6375,7 +6375,7 @@ class aboutWindow(wc.ToolWindows):
             from PyQt5.QtCore import QT_VERSION_STR
         from scipy import __version__ as scipyVersion
         self.text.setText('<p><b>ssNake ' + VERSION + '</b></p>' +
-                          '<p>Copyright (&copy;) 2016&ndash;2017 Bas van Meerten & Wouter Franssen<\p>' + '<p>Email: <a href="mailto:ssnake@science.ru.nl" >ssnake@science.ru.nl</a></p>' +
+                          '<p>Copyright (&copy;) 2016&ndash;2018 Bas van Meerten & Wouter Franssen<\p>' + '<p>Email: <a href="mailto:ssnake@science.ru.nl" >ssnake@science.ru.nl</a></p>' +
                           '<b>Library versions</b>:<br>Python ' + pythonVersion + '<br>numpy ' + np.__version__ +
                           '<br>SciPy ' + scipyVersion +
                           '<br>matplotlib ' + matplotlib.__version__ +
