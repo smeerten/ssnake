f0cfadb1655e63a00188df134ae88cb14e944b21
wfranssen <wmjfranssen@zoho.com>
2018-09-07 12:28:53 +0200

    Fixed monitor TODO: make it work for Combined data

--- src/ssNake.py
+++ src/ssNake.py
@@ -1764,14 +1764,14 @@ class Main1DWindow(QtWidgets.QWidget):
 
     def startMonitor(self, macroNames, delay=0.5):
         self.monitorMacros = macroNames
-        self.monitor = QtCore.QFileSystemWatcher(self.masterData.filePath[0], self)
+        self.monitor = QtCore.QFileSystemWatcher(self.masterData.filePath[0][0], self)
         self.monitor.fileChanged.connect(lambda a: self.monitorLoad(a, delay))
         self.monitor.directoryChanged.connect(lambda a: self.monitorLoad(a, delay))
 
     def stopMonitor(self):
         self.monitorMacros = []
         if self.monitor is not None:
-            self.monitor.removePath(self.masterData.filePath[0])
+            self.monitor.removePath(self.masterData.filePath[0][0])
         self.monitor = None
 
     def real(self):
