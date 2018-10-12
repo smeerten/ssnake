b2b3654fb4cb09e0f2ea40740c55ff017bbde5cc
bmeerten <smeerten@gmail.com>
2018-10-12 16:48:56 +0200

    Preparations for v0.7 release

--- src/ssNake.py
+++ src/ssNake.py
@@ -100,7 +100,7 @@ matplotlib.rc('font', family='DejaVu Sans')
 np.set_printoptions(threshold=np.nan)
 QtCore.QLocale.setDefault(QtCore.QLocale('en_US'))
 
-VERSION = 'v0.7b'
+VERSION = 'v0.7'
 # Required library version
 NPVERSION = '1.11.0'
 MPLVERSION = '1.4.2'
