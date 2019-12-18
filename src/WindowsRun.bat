e5669528f31b813c2685ebaa66326dc3108c146d
Franssen <wouter.franssen@wur.nl>
2019-12-18 14:19:07 +0100

    Update WindowsRun.bat to check for pyw or pythonw

--- src/WindowsRun.bat
+++ src/WindowsRun.bat
@@ -1,3 +1,17 @@
 set mypath=%cd%
 cd ..\..
-start pyw "%mypath%"\ssNake.py
+where /q pyw
+IF ERRORLEVEL 1 (
+    ECHO 'pyw' is missing, try 'pythonw'
+    where /q pythonw
+    IF ERRORLEVEL 1 (
+        ECHO 'pythonw' also missing. Abort.
+    ) ELSE (
+       start pythonw "%mypath%"\ssNake.py
+    )
+) ELSE (
+    start pyw "%mypath%"\ssNake.py
+)
+
+
+
