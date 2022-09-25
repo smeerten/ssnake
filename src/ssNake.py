66a9809f00e1d70b0e25428b40d7f0ea0125220a
bmeerten <smeerten@gmail.com>
2022-09-25 12:25:41 +0200

    Start v1.5b development

--- src/ssNake.py
+++ src/ssNake.py
@@ -96,7 +96,7 @@ matplotlib.rc('font', family='DejaVu Sans')
 np.set_printoptions(threshold=sys.maxsize)
 QtCore.QLocale.setDefault(QtCore.QLocale('en_US'))
 
-VERSION = 'v1.4'
+VERSION = 'v1.5b'
 # Required library version
 NPVERSION = '1.11.0'
 MPLVERSION = '1.5.0'
