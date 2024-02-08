823b2d95218aa031889b6cc3e5d59654440483f2
Julien TREBOSC <julien.trebosc@univ-lille.fr>
2024-02-08 16:15:50 +0100

    Fix/Enhance WindowsRun.bat run options
    A bug occurs if %mypath% contains " because the start command assumes
    that if the first argument is quotedi "" then it is the label of
    the terminal. In that case %curdir% is consumed as a label and ssnake.py
    is not found. The fix is simply to define explicitely the terminal label.
    For WindowsRun.bat script to work, pythonw or pyw must be in the search path.
    During anaconda/miniconda python installation adding path to miniconda in
    users environment path is not recommended. This could clash with other python
    installations.
    An alternative is to define a variable SSNAKE4PYTHON in user environement
    variables that points to the required python executable. If such variable
    exists it is used in priority, otherwise old behavior is used.

--- src/WindowsRun.bat
+++ src/WindowsRun.bat
@@ -1,17 +1,35 @@
+REM Check for a user defined environnment variable (PYTHON4SSNAKE) containing the location of python interpreter.
+REM Else this script will start ssNake only if pyw.exe or pythonw.exe is found in current path search. 
+REM Otherwise it will quit
+@echo off
 set mypath=%cd%
 cd ..\..
-where /q pyw
-IF ERRORLEVEL 1 (
-    ECHO 'pyw' is missing, try 'pythonw'
-    where /q pythonw
-    IF ERRORLEVEL 1 (
-        ECHO 'pythonw' also missing. Abort.
-    ) ELSE (
-       start pythonw "%mypath%"\ssNake.py
-    )
-) ELSE (
-    start pyw "%mypath%"\ssNake.py
+
+IF DEFINED PYTHON4SSNAKE (
+set PYTHONW=%PYTHON4SSNAKE%
+goto run_ssnake
 )
 
+where /q pyw
+IF ERRORLEVEL 1 (
+    ECHO 'pyw' not in search path, try 'pythonw'
+) ELSE (
+    SET PYTHONW=pyw
+    goto run_ssnake
+)
 
+where /q pythonw
+IF ERRORLEVEL 1 (
+    ECHO 'pythonw' also not in search path.
+) ELSE (
+    SET PYTHONW=pythonw
+    goto run_ssnake
+)
+ECHO No python interpreter found: about to leave
+PAUSE
+EXIT
+
+:run_ssnake
+REM Let's try running ssNake !!!!
+start "ssNake" %PYTHONW% "%mypath%"\ssNake.py
 
